b91c1c2bc85ef574489014c4ade48ee689288bd2
Daulet Tulepbergenov <daulet6368@gmail.com>
2024-01-07 00:30:01 +0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  return (
+    <main className="flex-fill">
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <header>
+                <h1>Hello World</h1>
+              </header>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
